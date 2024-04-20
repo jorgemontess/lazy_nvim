ad56475b6d8ede822c6aac44bf1621381397d50b
Jorge Montes <jomontes97@hotmail.com>
2024-04-20 17:45:33 -0400

    Functional version

--- lua/jorge/lazy/treesitter.lua
+++ lua/jorge/lazy/treesitter.lua
@@ -6,7 +6,23 @@ return{
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          ensure_installed = { 
+                "c",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "javascript",
+                "html",
+                "typescript",
+                "go",
+                "scss",
+                "sql",
+                "php",
+                "http",
+                "gitcommit",
+                "gitignore",
+            },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
