4c8d1b46b1eda58c4d43273a68cf72e1fbf8ba9c
Jorge Montes <jomontes97@hotmail.com>
2024-04-23 00:29:12 -0400

    adding cmp configurations but not completed

--- lua/jorge/lazy/lsp.lua
+++ lua/jorge/lazy/lsp.lua
@@ -1,5 +1,5 @@
-return{
-
+--TODO: Organizar cmp y lsp en diferentes files
+return {
     -- IMPORT HERE
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -12,65 +12,141 @@ return{
         "hrsh7th/nvim-cmp",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets",
+        "onsails/lspkind.nvim",
     },
-
     config = function()
+        -- CONFIGURATION HERE
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "rust_analyzer",
+                "tsserver",
+                "pyright",
+            }
+        })
 
-    -- CONFIGURATION HERE
+        
+        -- Setup language servers.
+        local lspconfig = require('lspconfig')
+        lspconfig.pyright.setup {}
+        lspconfig.tsserver.setup {}
+        lspconfig.rust_analyzer.setup {
+            -- Server-specific settings. See `:help lspconfig-setup`
+            settings = {
+                ['rust-analyzer'] = {},
+            },
+        }
+
+        -- Global mappings.
+        -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+        vim.keymap.set('n', '<space>f', vim.diagnostic.open_float)
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+        vim.keymap.set('n', '<space>l', vim.diagnostic.setloclist)
+
+        -- Use LspAttach autocommand to only map the following keys
+        -- after the language server attaches to the current buffer
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+                -- Enable completion triggered by <c-x><c-o>
+                vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                -- Buffer local mappings.
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                local opts = { buffer = ev.buf }
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+
+                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+            end,
+        })
+        
+        -- CMP configuration
+        local cmp = require('cmp')
+        local lspkind = require('lspkind')
+
+        require('luasnip.loaders.from_vscode').lazy_load()
+        cmp.setup({
+
+            completion = {
+
+              completopt = "menu,menuone,preview,noselect",
+            },
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+            window = {},
+            mapping = cmp.mapping.preset.insert({
+                
+                ['<C-k>'] = cmp.mapping.select_prev_item(),   -- Select previous suggestion
+                ['<C-j>'] = cmp.mapping.select_next_item(),   -- Select next suggestion
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),   -- Show complete selections
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+                { name = 'path' },
+            }, {
+                { name = 'buffer' },
+            })
+        })
+
+        -- Set configuration for specific filetype.
+        cmp.setup.filetype('gitcommit', {
+            sources = cmp.config.sources({
+                { name = 'git' },
+            }, {
+                { name = 'buffer' },
+            })
+        })
       
-      --TODO: Bug "server rust analyzer is not a valid entry in ensure installed"
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          --"rust-analyzer",
-          "pyright",
-      }
-    })
-    
-      -- Setup language servers.
-      local lspconfig = require('lspconfig')
-      lspconfig.pyright.setup {}
-      lspconfig.tsserver.setup {}
-      lspconfig.rust_analyzer.setup {
+        formatting = {
+          format = lspkind.cmp_format({
+            maxwidth = 50,
+            ellipsis_char = "...",
+          })
+         },
 
-       -- Server-specific settings. See `:help lspconfig-setup`
-        settings = {
-          ['rust-analyzer'] = {},
-        },
-      }
+        -- Use buffer source for `/` and `?`
+        cmp.setup.cmdline({ '/', '?' }, {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' }
+            }
+        })
+
+        -- Use cmdline & path source for ':'
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' }
+            }, {
+                { name = 'cmdline' }
+            }),
+            matching = { disallow_symbol_nonprefix_matching = false }
+        })
+
+        -- Set up lspconfig.
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+        require('lspconfig')['tsserver'].setup {
+            capabilities = capabilities
+        }
 
 
-      -- Global mappings.
-      -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-      vim.keymap.set('n', '<space>f', vim.diagnostic.open_float)
-      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-      vim.keymap.set('n', '<space>l', vim.diagnostic.setloclist)
-
-      -- Use LspAttach autocommand to only map the following keys
-      -- after the language server attaches to the current buffer
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-        callback = function(ev)
-          -- Enable completion triggered by <c-x><c-o>
-          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-          -- Buffer local mappings.
-          -- See `:help vim.lsp.*` for documentation on any of the below functions
-          local opts = { buffer = ev.buf }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-
-          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-
-        end,
-      })
-    
     end
 }
--- lua/jorge/remap.lua
+++ lua/jorge/remap.lua
@@ -26,8 +26,8 @@ vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Better window navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
-vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
-vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+--vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+--vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
 vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
 
 
