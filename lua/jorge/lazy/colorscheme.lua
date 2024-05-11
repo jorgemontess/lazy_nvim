02efa2387ff664c476daa83da5c50b36815165cb
Jorge Montes <jomontes97@hotmail.com>
2024-05-11 02:20:59 -0400

    some configurations in colorscheme

--- lua/jorge/lazy/colorscheme.lua
+++ lua/jorge/lazy/colorscheme.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
+	color = color or "kanagawa"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -81,7 +81,7 @@ return {
                 })
 
                 --SET COLORSCHEME
-                vim.cmd("colorscheme rose-pine")
+                vim.cmd("colorscheme kanagawa")
 
                 ColorMyPencils()
 
@@ -136,4 +136,44 @@ return {
 
     },
 
+    {
+        --ROSE-PINE COLORSCHEME
+        --
+        "rebelot/kanagawa.nvim",
+
+            config = function()
+                require('kanagawa').setup({
+
+                    compile = false,             -- enable compiling the colorscheme
+                    undercurl = true,            -- enable undercurls
+                    commentStyle = { italic = true },
+                    functionStyle = {},
+                    keywordStyle = { italic = true},
+                    statementStyle = { bold = true },
+                    typeStyle = {},
+                    transparent = true,         -- do not set background color
+                    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+                    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+                    colors = {                   -- add/modify theme and palette colors
+                        palette = {},
+                        theme = { wave = {}, lotus = {}, dragon = {},
+                            all = { ui = { bg_gutter = "none"} }
+                        },
+                    },
+                    overrides = function(colors) -- add/modify highlights
+                        local theme = colors.theme
+                        return {
+                            FloatBorder = { fg = "none", bg = "none" },
+                            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+                        }
+                    end,
+                    theme = "wave",              -- Load "wave" theme when 'background' option is not set
+                    background = {               -- map the value of 'background' option to a theme
+                        dark = "wave",           -- try "dragon" !
+                        light = "lotus"
+                    },
+                })
+
+            end
+    },
 }
