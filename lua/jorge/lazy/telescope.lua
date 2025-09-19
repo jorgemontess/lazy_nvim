177b7ff9ff8f8a7f5ef6a7a5cc5a53791a42c2fd
Jorge Montes <jomontes97@hotmail.com>
2025-09-18 20:57:11 -0500

    feat: add new keybind for live grep withou regex

--- lua/jorge/lazy/telescope.lua
+++ lua/jorge/lazy/telescope.lua
@@ -1,21 +1,37 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' },
+  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+  dependencies = { 'nvim-lua/plenary.nvim' },
 
-   config = function()
+  config = function()
+    require('telescope').setup({
+      defaults = {
+        use_less = true,
+        -- opcional: ignora .git al buscar
+        -- vimgrep_arguments = {
+        --   'rg', '--color=never', '--no-heading', '--with-filename',
+        --   '--line-number', '--column', '--smart-case',
+        --   '--hidden', '--glob', '!.git/'
+        -- },
+      },
+    })
 
-       require('telescope').setup({
-         defaults = {
-          use_less = true,
-         },
-       })
+    local builtin = require('telescope.builtin')
 
-       local builtin = require('telescope.builtin')
-          vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-          vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-          vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-          vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-          vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
-       end,
+    -- Tus atajos originales
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>pg', builtin.live_grep, {}) -- (regex)
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+
+    -- 🔎 NUEVO: Live Grep "literal" (sin regex) -> busca env( sin tener que escapar
+    vim.keymap.set('n', '<leader>pl', function()
+      builtin.live_grep({
+        additional_args = function()
+          return { '--fixed-strings' }  -- igual que -F
+        end
+      })
+    end, { desc = 'Live grep (literal, sin regex)' })
+  end,
 }
+
